426dec2f66d3a2b4ddeda4a82d38e0ea0fa96d61
Andrea Zorzi <info@zorziandrea.com>
2025-08-31 10:43:19 +0200

    Add replace tab command

--- src/extension.ts
+++ src/extension.ts
@@ -10,6 +10,46 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "tab-replace" is now active!');
 
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	const disposable = vscode.commands.registerCommand('tab-replace.replace', () => {
+		const editor = vscode.window.activeTextEditor;
+        
+        if (!editor) {
+            vscode.window.showErrorMessage('No active editor found');
+            return;
+        }
+
+        const document = editor.document;
+        const fullRange = new vscode.Range(
+            document.positionAt(0),
+            document.positionAt(document.getText().length)
+        );
+        
+        const text = document.getText();
+        const tabsCount = (text.match(/\t/g) || []).length;
+        
+        if (tabsCount === 0) {
+            // vscode.window.showInformationMessage('No tabs found in the document');
+            return;
+        }
+
+        // Replace all tabs with 4 spaces
+		const tabSize = vscode.workspace.getConfiguration('editor', editor.document.uri).get<number>('tabSize') || 4;
+        const newText = text.replace(/\t/g, ' '.repeat(tabSize));
+        
+        editor.edit(editBuilder => {
+            editBuilder.replace(fullRange, newText);
+        }).then(success => {
+            if (success) {
+                vscode.window.showInformationMessage(`Converted ${tabsCount} tabs to spaces`);
+            } else {
+                vscode.window.showErrorMessage('Failed to convert tabs to spaces');
+            }
+        });
+	});
+
 	context.subscriptions.push(disposable);
 }
 
